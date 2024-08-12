ebd95292b0b1aad80a454e695d2e55644d14c3aa
lefmorgan <lef.morgan@gmail.com>
2024-08-12 11:19:18 +0200

    table-boradgame

--- projects/boardgameotheque/src/app/features/boardgames/components/table-boardgames/table-boardgames.component.ts
+++ projects/boardgameotheque/src/app/features/boardgames/components/table-boardgames/table-boardgames.component.ts
@@ -1,15 +1,33 @@
-import { Component, input } from '@angular/core';
+import { Component, input, WritableSignal } from '@angular/core';
 import { Boardgame } from '../../models/boardgame';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'mbg-table-boardgames',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatFormFieldModule, MatInputModule,],
   templateUrl: './table-boardgames.component.html',
   styleUrl: './table-boardgames.component.scss'
 })
 export class TableBoardgamesComponent {
   items = input.required<Boardgame[]>();
   title = input<string>('');
+
+  displayedColumns: string[] = ['position', 'thumbnail', 'name', 'yearPublished'];
+
+  dataSource = new MatTableDataSource<Boardgame>();
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.dataSource.data = this.items();
+    }, 1000);
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
