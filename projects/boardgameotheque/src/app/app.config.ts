6199c1f522b09b24ffd3784c675ca312d5fd8d47
lefmorgan <lef.morgan@gmail.com>
2024-09-04 14:31:06 +0200

    add firebase

--- projects/boardgameotheque/src/app/app.config.ts
+++ projects/boardgameotheque/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
+  provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -8,10 +8,18 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore} from '@angular/fire/firestore';
+import { environment } from '../environments/environement';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes), provideAnimationsAsync(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    // firebase
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideFirestore(() => getFirestore()),
   ],
 };
---
+++ projects/boardgameotheque/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+   firebaseConfig: {
+    apiKey: "AIzaSyDZlmN5vfhhGqZyY-j9AzI9VZ_XpC--tTk",
+    authDomain: "boardgameotheque.firebaseapp.com",
+    projectId: "boardgameotheque",
+    storageBucket: "boardgameotheque.appspot.com",
+    messagingSenderId: "372012389447",
+    appId: "1:372012389447:web:ffd85b1a94a94c6e5f4d62"
+  }
+}
